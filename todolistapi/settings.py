88b38e59c742be7aaa91dddda4ad182c2d8b091c
MohGinanjar <ginanjar.farin@gmail.com>
2021-08-24 00:35:08 +0700

    create custom User

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+AUTH_USER_MODEL="authentication.User"
+
+
 
 # Application definition
 
@@ -38,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'authentication',
+    'todos',
 ]
 
 MIDDLEWARE = [
